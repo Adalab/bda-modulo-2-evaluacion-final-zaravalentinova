5d4a615b71c36365444d82aaffd71b19924df7e0
unknown <zaravalentinova@yahoo.es>
2025-07-10 11:19:02 +0200

    Ejercicio 1-10

---
+++ Evaluación_Final_Modulo_2_Zara_Valentinova_Pashova.sql
@@ -0,0 +1,96 @@
+############################
+##### EVALUACIÓN FINAL #####
+####### MÓDULO 2. SQL ######
+############################
+
+/* BASE DE DATOS SAKILA:
+Para este ejercicio utilizaremos la bases de datos Sakila que hemos estado utilizando durante el repaso de
+SQL. Es una base de datos de ejemplo que simula una tienda de alquiler de películas. Contiene tablas como
+film (películas), actor (actores), customer (clientes), rental (alquileres), category (categorías), entre
+otras. Estas tablas contienen información sobre películas, actores, clientes, alquileres y más, y se utilizan
+para realizar consultas y análisis de datos en el contexto de una tienda de alquiler de películas. */
+
+USE sakila;
+
+-- EJERCICIO 1. Selecciona todos los nombres de las películas sin que aparezcan duplicados.
+SELECT DISTINCT title
+	FROM film; -- Nota: no hay nombres de películas duplicados.
+
+-- EJERCICIO 2. Muestra los nombres de todas las películas que tengan una clasificación de "PG-13".
+SELECT title
+	FROM film
+    WHERE rating="PG-13";
+
+-- EJERCICIO 3:  Encuentra el título y la descripción de todas las películas que contengan la palabra "amazing" en su descripción.
+SELECT title, `description`
+	FROM film
+    WHERE `description` LIKE "%amazing%"; -- Nota: "%amazing%" significa cualquier cosa antes y después de amazing o nada, por lo que sería redundante incluir OR.
+
+-- EJERCICIO 4: Encuentra el título de todas las películas que tengan una duración mayor a 120 minutos.
+SELECT title
+	FROM film
+    WHERE length > 120;
+
+-- EJERCICIO 5: Recupera los nombres de todos los actores.
+SELECT first_name
+	FROM actor;
+
+-- EJERCICIO 6: Encuentra el nombre y apellido de los actores que tengan "Gibson" en su apellido.
+SELECT first_name, last_name
+	FROM actor
+    WHERE last_name LIKE "%Gibson%";
+    
+-- Otra posibilidad:
+SELECT first_name, last_name
+	FROM actor
+    WHERE last_name REGEXP "Gibson";
+
+-- EJERCICIO 7: Encuentra los nombres de los actores que tengan un actor_id entre 10 y 20.
+SELECT first_name, actor_id
+	FROM actor
+	WHERE actor_id BETWEEN 10 AND 20; -- Nota: otra manera de hacerlo sería con WHERE actor_id>=10 AND actor_id<=20; Duda: El resultado muestra un registro con valor NULL (posiblemente debido a algún join anteriormente creado), no consigo excluir ese registro (he probado con "AND first_name IS NOT NULL" y otras condiciones similares).
+
+-- EJERCICIO 8: Encuentra el título de las películas en la tabla film que no sean ni "R" ni "PG-13" en cuanto a su clasificación.
+SELECT title
+	FROM film
+    WHERE NOT rating="R" AND NOT rating="PG-13"; -- Nota: usamos AND dado que ambas condiciones se tienen que cumplir a la vez.
+    
+ -- Otra posibilidad: 
+SELECT title
+	FROM film
+	WHERE rating <> "R" AND rating <> "PG-13";
+
+-- EJERCICIO 9: Encuentra la cantidad total de películas en cada clasificación de la tabla film y muestra la clasificación junto con el recuento.      
+SELECT COUNT(title) AS Recuento, rating AS Clasificación
+	FROM film
+	GROUP BY Clasificación;    
+
+-- EJERCICIO 10: Encuentra la cantidad total de películas alquiladas por cada cliente y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.
+SELECT c.customer_id, c.first_name, c.last_name, COUNT(f.title) AS total_peliculas
+	FROM customer AS c
+	INNER JOIN rental AS r ON c.customer_id = r.customer_id -- INNER JOIN porque queremos solo los clientes que han alquilado películas.
+	INNER JOIN inventory AS i ON r.inventory_id = i.inventory_id 
+	INNER JOIN film AS f ON i.film_id = f.film_id -- INNER JOIN porque queremos solo las películas que están en el inventario.
+    GROUP BY c.customer_id;
+
+-- EJERCICIO 11: Encuentra la cantidad total de películas alquiladas por categoría y muestra el nombre de la categoría junto con el recuento de alquileres.
+SELECT COUNT(f.film_id), f.rating, COUNT(r.rental_id)
+	FROM film AS f
+	INNER JOIN inventory AS i
+	INNER JOIN rental AS r
+	GROUP BY r.rental_id, f.film_id;
+
+
+SELECT * FROM rental;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
